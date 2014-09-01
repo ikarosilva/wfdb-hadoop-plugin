9689314f7d67a1a2b7eb73ea50694b1bf663f406
ikarosilva00@gmail.com <ikarosilva00@gmail.com@3082b230-b982-3a75-afe3-03b4d68ae009>
2014-09-01 23:40:14 +0000

    changes to work with mghdb

--- wfdb-hadoop-plugin/trunk/prepare-dataset.sh
+++ wfdb-hadoop-plugin/trunk/prepare-dataset.sh
@@ -59,7 +59,7 @@ fi
 source wfdb-hadoop-configuration.sh
 
 #Database name to push to HDFS                                                                                                                       
-DB=mimicdb2
+DB=mghdb
 
 #Set this flag to true for generating a data set to process in LOCAL_MODE
 #Set this flag to false for generating a data set to be processed in STREAMING MODE
